a391ff7317cc2dcaee3b36b929e7579962f33e7f
bates phillip <58259phil@gmail.com>
2026-05-22 19:16:00 +1000

    increase padding on tinylaunch badge

--- src/App.jsx
+++ src/App.jsx
@@ -300,7 +300,7 @@ export default function App() {
                   <img alt="AI Stack Builder on Product Hunt" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1137974&theme=light&t=1779438219252" style={{ height: '40px', width: 'auto', display: 'block' }} />
                 </a>
                 <a href="https://tinylaunch.com/launch/13163" target="_blank" rel="noopener">
-                  <img src="https://tinylaunch.com/tinylaunch_badge_featured_on.svg" alt="Featured on TinyLaunch" style={{ height: '40px', width: 'auto', display: 'block' }} />
+                  <img src="https://tinylaunch.com/tinylaunch_badge_featured_on.svg" alt="Featured on TinyLaunch" style={{ height: '40px', width: 'auto', display: 'block', padding: '4px 0', boxSizing: 'border-box' }} />
                 </a>
               </div>
             </div>
